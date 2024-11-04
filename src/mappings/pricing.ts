380af656e3f8e3daa079654996b48e3155e87821
mguleryuz <mguleryuz3@gmail.com>
2024-11-03 21:20:22 -0500

    fix: try_getPair instead of get pair

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -80,7 +80,13 @@ export function findEthPerToken(token: Token): BigDecimal {
   }
   // loop through whitelist and check if paired with any
   for (let i = 0; i < WHITELIST.length; ++i) {
-    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+    let pairAddressResult = factoryContract.try_getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+
+    if (pairAddressResult.reverted) {
+      continue
+    }
+
+    let pairAddress = pairAddressResult.value
     if (pairAddress.toHexString() != ADDRESS_ZERO) {
       let pair = Pair.load(pairAddress.toHexString())
       if (pair === null) {
@@ -91,18 +97,18 @@ export function findEthPerToken(token: Token): BigDecimal {
         if (token1 === null) {
           continue
         }
-        return pair.token1Price.times(token1.derivedETH as BigDecimal) // return token1 per our token * Eth per token 1
+        return pair.token1Price.times(token1.derivedETH as BigDecimal)
       }
       if (pair.token1 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
         let token0 = Token.load(pair.token0)
         if (token0 === null) {
           continue
         }
-        return pair.token0Price.times(token0.derivedETH as BigDecimal) // return token0 per our token * ETH per token 0
+        return pair.token0Price.times(token0.derivedETH as BigDecimal)
       }
     }
   }
-  return ZERO_BD // nothing was found return 0
+  return ZERO_BD
 }
 
 /**
